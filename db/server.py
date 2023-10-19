74e70c2e89d685680b2a14ecec09276966ee3b14
Grant-S-Clark <grantsamuelclark@gmail.com>
2023-10-19 17:32:21 -0400

    made file for database server client

---
+++ db/server.py
@@ -0,0 +1,10 @@
+import socket
+
+if __name__ == "__main__":
+    # create an INET STREAMING socket.
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # bind the socket to a public host with the port 8000.
+    serversocket.bind((socket.gethostname(), 8000))
+    
+    serversocket.listen(1) # only allow one connection (the master program)
