0d1f2a9b8003c6ad0ffecd5efaf0c4111697f4be
Grant-S-Clark <grantsamuelclark@gmail.com>
2023-10-26 11:35:12 -0400

    Set up database and made a function to get all data from database to send to a client.

--- db/server.py
+++ db/server.py
@@ -1,39 +1,110 @@
-import socket
-from Communicator import Communicator
+# pip install --user twisted
+from twisted.internet import protocol, reactor
+import sqlite3
 
 PORT = 8000
+db = None # Will be set later.
 
-if __name__ == "__main__":
-    # create an INET STREAMING socket.
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Translate all tables into a dictionary of 2 dimensional lists.
+"""
+ret["TT"] = tournament table list
+ret["MT"] = match table list
+ret["GT"] = game table list
+ret["PT"] = player table list
+ret["BT"] = match binary tree table list
+"""
+def fetchall():
+    global db
+    cur = db.cursor()
     
-    # bind the socket to a public host with the defined PORT value.
-    hostname = socket.gethostname()
-    serversocket.bind((hostname, PORT))
+    ret = dict()
 
-    # Print the hostname and port for the user.
-    print(f"LISTENING ON {(hostname, PORT)}")
+    cur.execute("SELECT * FROM tournaments")
+    ret["TT"] = list()
+    for row in cur:
+        ret["PT"].append(list(row))
+
+    cur.execute("SELECT * FROM matches")
+    ret["MT"] = list()
+    for row in cur:
+        ret["PT"].append(list(row))
+
+    cur.execute("SELECT * FROM games")
+    ret["GT"] = list()
+    for row in cur:
+        ret["PT"].append(list(row))
     
-    serversocket.listen(1) # only allow one connection at a time (the master program)
+    cur.execute("SELECT * FROM players")
+    ret["PT"] = list()
+    for row in cur:
+        ret["PT"].append(list(row))
 
-    connection, address = serversocket.accept() # Client program has connected
-    print(f"CLIENT CONNECTED.")
+    cur.execute("SELECT * FROM match_tree")
+    ret["BT"] = list()
+    for row in cur:
+        ret["PT"].append(list(row))
     
-    comm = Communicator(serversocket)
+    return ret
 
-    try:
-        client_msg = comm.recv_msg()
-    except OSError:
-        print("CLIENT SOCKET CLOSED.")
-        serversocket.shutdown()
-        serversocket.close()
-        quit()
+class SimpleServer(protocol.Protocol):
+    def connectionMade(self):
+        print("Client has connected to the server.")
+
+    def connectionLost(self, reason):
+        print ("Lost connection with client.")
         
-    print(f"RECIEVED: \"{client_msg}\"")
+    def dataReceived(self, data):
+        # Process inputs, get outputs.
+        "As soon as any data is received, write it back."
+        self.transport.write(str(fetchall()).encode())
 
-    comm.send_msg("Message Recieved.")
+def init():
+    global db
+    db = sqlite3.connect("rppcs_data.db")
+    cur = db.cursor()
+    # Tournament table
+    cur.execute("""CREATE TABLE IF NOT EXISTS tournaments(id     INT UNSIGNED NOT NULL,
+                                                          name   TEXT NOT NULL,
+                                                          date   DATE NOT NULL)""")
 
-    serversocket.shutdown()
-    serversocket.close()
+    # Match table
+    cur.execute("""CREATE TABLE IF NOT EXISTS matches(id      INT UNSIGNED NOT NULL,
+                                                      t_id    INT UNSIGNED NOT NULL,
+                                                      p1_id   INT UNSIGNED NOT NULL,
+                                                      p2_id   INT UNSIGNED NOT NULL)""")
+
+    # Game table
+    cur.execute("""CREATE TABLE IF NOT EXISTS games(id         INT UNSIGNED NOT NULL,
+                                                    m_id       INT UNSIGNED NOT NULL,
+                                                    p1_score   TINYINT UNSIGNED NOT NULL,
+                                                    p2_score   TINYINT UNSIGNED NOT NULL)""")
+
+    # Player table
+    cur.execute("""CREATE TABLE IF NOT EXISTS players(id      INT UNSIGNED,
+                                                      name    TEXT NOT NULL,
+                                                      skill   SMALLINT UNSIGNED NOT NULL)""")
+
+    # Match binary tree
+    cur.execute("""CREATE TABLE IF NOT EXISTS match_tree(parent_id      INT UNSIGNED NOT NULL,
+                                                         l_child_id     INT UNSIGNED NOT NULL,
+                                                         r_child_id     INT UNSIGNED NOT NULL)""")
+    db.commit()
+    
+    # Insert the null player into the players table if necessary.
+    # Null player will have an id of 0.
+    res = cur.execute("SELECT * FROM players WHERE id = 0")
+    if res.fetchone() is None:
+        cur.execute("INSERT INTO players (id, name, skill) VALUES (0, 'N/A', 0)")
+        
+    db.commit()
     
     
+if __name__ == "__main__":
+    init() # Set up database if necessary.
+    
+    # This runs the protocol on port 8000
+    factory = protocol.ServerFactory() # Basic server factory.
+    factory.protocol = SimpleServer
+    reactor.listenTCP(PORT, factory)
+    reactor.run()
+
--- db/test_client.py
+++ db/test_client.py
@@ -1,20 +1,37 @@
-import socket
-from Communicator import Communicator
+from twisted.internet import protocol, reactor
 
+PORT = 8000
+# a client protocol
+
+class SimpleClient(protocol.Protocol):
+    """Once connected, send a message, then print the result."""
+
+    def connectionMade(self):
+        self.transport.write(b"hello, world!")
+
+    def dataReceived(self, data):
+        # As soon as any data is received, print it and close connection.
+        print("Server said:", data.decode())
+        self.transport.loseConnection()
+
+    def connectionLost(self, reason):
+        print(f"connection lost")
+
+
+class SimpleFactory(protocol.ClientFactory):
+    protocol = SimpleClient
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Connection failed!")
+        print(reason)
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print("Connection lost - goodbye!")
+        reactor.stop()
+
+# this only runs if the module was *not* imported
 if __name__ == "__main__":
-    address = input("ADDRESS: ")
-    port = int(input("PORT: "))
-
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    clientsocket.connect((address, port))
-
-    comm = Communicator(clientsocket)
-
-    comm.send_msg("Hello World!")
-    reply = comm.recv_msg()
-
-    print(f"REPLY RECIEVED: \"{reply}\"")
-    
-    clientsocket.shutdown()
-    clientsocket.close()
+    f = SimpleFactory()
+    reactor.connectTCP("localhost", PORT, f)
+    reactor.run()
